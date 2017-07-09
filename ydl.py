1b4cc8290eba260efffd6594d511f9fd2d4f92e0
shibli049 <shibli05@gmail.com>
2017-07-09 17:55:15 +0600

    added caching for last 100 urls

--- ydl.py
+++ ydl.py
@@ -12,7 +12,8 @@ import pafy
 from flask import Flask, render_template_string, url_for, redirect, request
 import os
 import logging
-
+import json
+from functools import lru_cache
 logging.basicConfig(level=logging.INFO,
                     format=' %(asctime)s - %(levelname)s - %(message)s')
 
@@ -20,11 +21,53 @@ app = Flask(__name__)
 app.debug = False
 app.config.from_object(__name__)
 TOKEN = str(os.environ.get('TOKEN', 'Test1234'))
-print("TOKEN:", TOKEN)
+logging.info("TOKEN:" + str(TOKEN))
+
+
+class YoutubeUrlMetadata:
+    def __init__(self, passed_url):
+        url=passed_url
+        logging.debug(1)
+        video = pafy.new(url)
+        logging.debug(2)
+        self.thumb = video.thumb
+        logging.debug(3)
+        self.title = video.title
+        logging.debug(4)
+        self.author = video.author
+        logging.debug(5)
+        self.rating = str(round(video.rating,4))
+        logging.debug(6)
+        self.viewcount = video.viewcount
+        logging.debug(7)
+        self.likes = video.likes
+        logging.debug(8)
+        self.dislikes = video.dislikes
+        logging.debug(9)
+        self.duration = str(video.duration)
+        logging.debug(10)
+        self.description = video.description
+        logging.debug(11)
+        self.vstreams = []
+        streams = video.streams
+        for s in streams:
+            self.vstreams.append({'resolution':s.resolution, 'extension': s.extension, 'file_size' : str(s.get_filesize()/1000) + 'KB', 'url' : s.url  })
+
+        self.astreams = []
+        streams = video.audiostreams
+        for s in streams:
+            self.astreams.append({'bitrate':s.bitrate, 'extension': s.extension, 'file_size' : str(s.get_filesize()/1000) + 'KB', 'url' : s.url  })
+
+@lru_cache(maxsize=100)
+def getYoutubeUrlMetadataJson(passed_url):
+
+    pyum = YoutubeUrlMetadata(passed_url)
+    return pyum
+    
 
 def generate_youtube_link(passed_url):
-    url=passed_url
-    video = pafy.new(url)
+    url = passed_url
+    video = getYoutubeUrlMetadataJson(url)
     str_output='''
     <div class="well">
         <div class="media">
@@ -34,7 +77,7 @@ def generate_youtube_link(passed_url):
     str_output=str_output+thumb+"</div><div class='media-body'>"
     title="<h4 class='media-heading'>Title: "+video.title+"</h4>"
     author="<b>Author: </b>"+video.author+"<br>"
-    rating_str=str(round(video.rating,4))
+    rating_str=video.rating
     rating="<b>Rating: </b>"+rating_str
     view_count="<b>Total view: </b>"+str(video.viewcount)
     like="<b>Like: </b>" + str(video.likes)
@@ -46,16 +89,18 @@ def generate_youtube_link(passed_url):
     str_output=str_output+description+"</div></div></div>"
 
     my_str="<h1>Video Links</h1><table class='table table-hover table-bordered table-striped'><tr><th>RESOLUTION</th><th>EXTENSION</th><th>FILESIZE</th><th>URL</th></tr>"
-    streams=video.streams
+    streams=video.vstreams
+
+    
     for s in streams:
-        my_str=my_str+"<tr><td>"+s.resolution+"</td><td>"+s.extension+"</td><td>"+str(s.get_filesize()/1000)+" KB</td><td><a download href="+s.url+">Download link</a></td></tr>"
+        my_str=my_str+"<tr><td>"+s['resolution']+"</td><td>"+s['extension']+"</td><td>"+s['file_size']+" </td><td><a download href="+s['url']+">Download link</a></td></tr>"
 
     str_output=str_output+my_str+"</table>"
 
     my_aud_str="<hr/><h1>Audio Links</h1><table class='table table-hover table-bordered table-striped'><tr><th>BITRATE</th><th>EXTENSION</th><th>FILESIZE</th><th>URL</th></tr>"
-    streams=video.audiostreams
+    streams=video.astreams
     for s in streams:
-        my_aud_str=my_aud_str+"<tr><td>"+s.bitrate+"</td><td>"+s.extension+"</td><td>"+str(s.get_filesize()/1000)+" KB</td><td><a download href="+s.url+">Download link</a></td></tr>"
+        my_aud_str=my_aud_str+"<tr><td>"+s['bitrate']+"</td><td>"+s['extension']+"</td><td>"+s['file_size']+" </td><td><a download href="+s['url']+">Download link</a></td></tr>"
     str_output=str_output+my_aud_str+"</table>"
 
 
@@ -83,7 +128,7 @@ def index():
         			  border-radius: 4px;
         			}
                     #youtube_link{
-                      width:550px;
+                      width:450px;
                     }
         		</style>
         	</head>
@@ -98,9 +143,9 @@ def index():
         str_form='''
     			<form class="form-inline" action="/" method="GET">
                     <label class="sr-only" for="youtube_link">YouTube Link:</label>
-                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                    <div class="input-group">
                         <div class="input-group-addon">YouTube Link:</div>
-                        <input name="youtubelink" type="text" class="form-control" id="youtube_link" placeholder="Paste YouTube link Here.">
+                        <input name="youtubelink" type="text" class="form-control col-6" id="youtube_link" placeholder="Paste YouTube link Here.">
                     </div>
                     <label class="sr-only" for="token">Token</label>
                     <div class="input-group mb-2 mr-sm-2 mb-sm-0">
@@ -111,11 +156,12 @@ def index():
                     <button onclick="window.location = window.location.pathname" type="button" class="btn btn-default"><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span></button>
                 </form>
         ''';
-        # print("werwer" , request.args)
+        # logging.debug("werwer" , request.args)
         if request.args.get('savebtn','').strip():
             youtubelink = request.args.get('youtubelink', '').strip()
             token = request.args.get('token', '').strip()
-            # print(token, TOKEN)
+            logging.debug(token + ' ' + TOKEN)
+            
             if(token != TOKEN):
                 return render_template_string(str_header+str_form+str_footer)
             returned_str=generate_youtube_link(youtubelink)
@@ -124,9 +170,18 @@ def index():
         else:
             return render_template_string(str_header+str_form+str_footer)
 
-if __name__ == "__main__":
+
+
+def jsonTest():
+    logging.debug(getYoutubeUrlMetadataJson("https://www.youtube.com/watch?v=JlS5zLUTZro"))
+    logging.debug(getYoutubeUrlMetadataJson("https://www.youtube.com/watch?v=JlS5zLUTZro"))
+    logging.debug(getYoutubeUrlMetadataJson("https://www.youtube.com/watch?v=JlS5zLUTZro"))
+
+
+def main():
     PORT = int(os.environ.get('PORT', 8080))
-    print("Open http://localhost:" + str(PORT) + "/ from browser")
-    print(dir(request))
+    logging.debug("Open http://localhost:" + str(PORT) + "/ from browser")
     app.run(host='0.0.0.0', port=PORT)
     
+if __name__ == "__main__":
+    main()
